6a9f5960602d8744194c4212a41ed3f2dd7e402c
Elismar Sivla <elismarsilva.5@outlook.com>
2020-04-29 18:48:16 -0300

    refactor(chore): homepage code refactored

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import './styles.css';
 
-import InferiorBar from '../../components/InferiorBar/InferiorBar';;
+import cards from './cards'
 
 export default class Home extends Component {
     render() {
@@ -13,24 +13,14 @@ export default class Home extends Component {
                             <p id="descricao">Uma plataforma para o monitoramento e controle de ambientes</p>
                     </div>
 
-                    <div className="Cards">
-                        <div className="Subcards">
-                            <h1>Controle seus aparelhos remotamente</h1>
-                            <p>Com o sinHome é possível controlar aparelhos a partir da internet de uma forma simples e interativa.</p>
-                        </div>
-                        <div className="Subcards">
-                            <h1>Economize energia</h1>
-                            <p>O modo de economia de energia possibilita o controle automático de aparelhos como ar condicionados e lâmpadas!</p>
-                        </div>
-                        <div className="Subcards">
-                            <h1>Informações em tempo real</h1>
-                            <p>Dados em tempo real hospeados na nuvem. Disponível para serem acessados nos demais dispositivos, desde celulares até desktops!</p>
-                        </div>
-                        <div className="Subcards">
-                            <h1>Documentação online e fácil instalação</h1>
-                            <p>Use o inHome hoje mesmo! Você só precisa de poucos materiais.</p>
-                        </div>
-                    </div>
+                    <section className="Cards">
+                        {cards.map((card) => (
+                            <div className="Subcards">
+                                <h1>{card.title}</h1>
+                                <p>{card.description}</p>
+                            </div>
+                        ))}
+                    </section>
                 </div>
         )
     }
---
+++ src/Pages/Home/cards.js
@@ -0,0 +1,20 @@
+const cards = [
+    {
+        title: "Controle seus aparelhos remotamente",
+        description: "Com o sinHome é possível controlar aparelhos a partir da internet de uma forma simples e interativa."
+    },
+    {
+        title: "Economize energia",
+        description: "O modo de economia de energia possibilita o controle automático de aparelhos como ar condicionados e lâmpadas!"
+    },
+    {
+        title: "Informações em tempo real",
+        description: "Dados em tempo real hospeados na nuvem. Disponível para serem acessados nos demais dispositivos, desde celulares até desktops!"
+    },
+    {
+        title: "Documentação online e fácil instalação",
+        description: "Use o inHome hoje mesmo! Você só precisa de poucos materiais."
+    },
+];
+
+export default cards;
