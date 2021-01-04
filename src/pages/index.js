47963c029d12bb9b877b19aec1e742c279ae1b71
Elismar13 <elismarsilva.5@outlook.com>
2021-01-04 12:03:03 -0300

    refactor(all): restarting project from zero using nextjs and styles components

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { GlobalStyle } from '../styles';
+
+// import SideBar from '../components/SuperiorBar';
+// import InferiorBar from '../components/InferiorBar';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      {/* <SideBar /> */}
+    </>
+  );
+}
+
+export default App;
