0a7b001f3b1f730e7651ed18ee37693504478d74
Elismar <elismarsilva.5@outlook.com>
2021-01-04 14:47:27 -0300

    style: 💄 global styles and _app file

---
+++ src/pages/_app.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background: white;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  :root {
+    --side-bar-color: #5DB9FF;
+  } 
+`;
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
